c681800fe4f8d9ebf0b27cd56ba9b5b72f68c930
Theodore Fobe <theodore.fobe@colorado.edu>
2020-02-14 00:42:19 -0600

    reimplemented zookeeper

---
+++ project_2/ZooKeeper.py
@@ -0,0 +1,35 @@
+from ABC import abc
+
+class ZooKeeper():
+    def __init__(name, zoo):
+        self.name = name
+        self.zoo = zoo
+
+    def wakeUpAnimals(self):
+        for animal in self.zoo:
+            print(self.name + " wakes up " + animal._name + " the " + animal._type + ".")
+
+    def rollCall(self):
+        print("-----  ROLL CALL  -----")
+        for animal in self.zoo:
+            animal.makeNoise()
+    
+    def feedTheAnimals(self):
+        print("-----  FEEDING TIME  -----")
+        for animal in self.zoo:
+            print(self.name + " feeds " + animal._name +  " the " + animal._type  + ".")
+            animal.eatFood()
+    
+    def exersizeTheAnimals(self):
+        for animal in self.zoo:
+            print(self.name + " exercises " + animal._name + " the " + animal._type + ".")
+            animal.roam()
+
+    def goToSleep(self):
+        for animal in self.zoo:
+            print(self.name + " puts " + animal._name + " the " + animal._type + " to sleep.")
+            animal.sleep()
+
+
+class ZooAnnoucer():
+    pass
